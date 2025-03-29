e1dc235f3ef21c357e5425129c5a1a7324efa274
fuuki2510 <fmoae37528@yahoo.co.jp>
2025-03-29 15:18:19 +0900

    課題終了

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>課題007</title>
+</head>
+
+<body>
+   <p>
+       <?php
+       // 連想配列に値を代入する
+       $eat_data = ['name' => 'onion', 'price' => 200, 'weight' => '160'];
+       print_r($eat_data);
+
+       ?>
+   </p>
+</body>
+
+</html>
